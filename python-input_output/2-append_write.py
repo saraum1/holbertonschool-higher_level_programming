4dfd38acda815a125963eadc03410121aa8f2728
saraum1 <saaraaaallls1@gmail.com>
2025-12-27 20:26:15 +0000

    Fix PEP8 E501 in 2-append_write

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module that provides a function to append a string to a UTF-8 text file."""
+"""Module for appending text to a file."""
 
 
 def append_write(filename="", text=""):
-    """Append a string at the end of a text file (UTF-8) and return char count."""
+    """Append a string at the end of a text file and return char count."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
